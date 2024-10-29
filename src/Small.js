418314448d29e33081271400be148a60b47d853c
agorapilled <johngardner13@outlook.com>
2024-10-29 17:31:19 -0400

    v0

--- src/Small.js
+++ src/Small.js
@@ -94,7 +94,7 @@ function Small({ handleCopy, copied, ca }) {
         <div className="bg-black text-white p-2 flex justify-between items-center rounded-t-md">
           <span className="font-bold font-mono text-xl text-red-600">Paranormal Doge</span>
           <div className='flex justify-center items-center z-10'>
-            <a href="https://x.com/" className=''>
+            <a href="https://x.com/pDOGEtoken" className=''>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 x="0px"
@@ -106,7 +106,7 @@ function Small({ handleCopy, copied, ca }) {
                 <path d="M 6.9199219 6 L 21.136719 26.726562 L 6.2285156 44 L 9.40625 44 L 22.544922 28.777344 L 32.986328 44 L 43 44 L 28.123047 22.3125 L 42.203125 6 L 39.027344 6 L 26.716797 20.261719 L 16.933594 6 L 6.9199219 6 z"></path>
               </svg>
             </a>
-            <a href="https://t.me/" className=''>
+            <a href="https://t.me/pDOGEportal" className=''>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 x="0px"
